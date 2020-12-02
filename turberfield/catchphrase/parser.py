b4eda28fb7f8705315e2b195bd2113fffb7b349a
tundish <tundish@gigeconomy.org.uk>
2020-12-02 19:20:22 +0000

    Add parser module and tests.

---
+++ turberfield/catchphrase/parser.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import enum
+import inspect
+import itertools
+
+
+class CommandParser:
+
+    discard = ("a", "an", "any", "her", "his", "my", "some", "the", "their")
+
+    @staticmethod
+    def unpack_annotation(name, annotation, ensemble):
+        terms = annotation if isinstance(annotation, list) else [annotation]
+        for t in terms:
+            if issubclass(t, enum.Enum):
+                yield from (
+                    (name, v) for i in t for v in (
+                        [i.value] if isinstance(i.value, str) else i.value
+                    )
+                )
+            else:
+                yield from ((name, i) for i in ensemble if isinstance(i, t))
+
+    @staticmethod
+    def parse_tokens(text, preserver=".", discard=None):
+        discard = discard or set()
+        return [i for i in text.rstrip(preserver).lower().split() if i not in discard or text.endswith(preserver)]
+
+    @staticmethod
+    def expand_commands(method, ensemble=[]):
+        """
+        Read a method's docstring and expand it to create all possible matching
+        command phrases. Calculate the corresponding keyword arguments.
+
+        Generates pairs of each command with a 2-tuple; (method, keyword arguments).
+
+        """
+        doc = method.func.__doc__ if hasattr(method, "func") else method.__doc__
+        terms = filter(None, (i.strip() for line in doc.splitlines() for i in line.split("|")))
+        params = list(itertools.chain(
+            list(CommandParser.unpack_annotation(p.name, p.annotation, ensemble))
+            for p in inspect.signature(method, follow_wrapped=True).parameters.values()
+            if p.annotation != inspect.Parameter.empty
+        ))
+        cartesian = [dict(i) for i in itertools.product(*params)]
+        for term in terms:
+            tokens = CommandParser.parse_tokens(term, discard=CommandParser.discard)
+            for prod in cartesian:
+                try:
+                    yield (" ".join(tokens).format(**prod).lower(), (method, prod))
+                except (AttributeError, IndexError, KeyError) as e:
+                    continue
---
+++ turberfield/catchphrase/test/test_parser.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+# This file is part of turberfield.
+#
+# Turberfield is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Turberfield is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import enum
+import unittest
+
+from turberfield.catchphrase.parser import CommandParser
+
+from turberfield.dialogue.types import DataObject
+
+
+class ParserTests(unittest.TestCase):
+
+    class Location(enum.Enum):
+        HERE = "here"
+        THERE = "there"
+
+    class Liquid(DataObject): pass
+    class Mass(DataObject): pass
+    class Space(DataObject): pass
+
+    def test_unpack_annotation_single_enum(self):
+        rv = list(CommandParser.unpack_annotation("locn", ParserTests.Location, ensemble=[]))
+        self.assertTrue(all(isinstance(i, tuple) for i in rv), rv)
+        self.assertEqual(
+            len([v for i in ParserTests.Location for v in ([i.value] if isinstance(i.value, str) else i.value)]),
+            len(rv),
+            rv
+        )
+
+    def test_unpack_annotation_enum_list(self):
+        class Season(enum.Enum):
+            spring = "Spring"
+            summer = "Summer"
+            autumn = "Autumn"
+            winter = "Winter"
+
+        rv = list(CommandParser.unpack_annotation("item", [ParserTests.Location, Season], ensemble=[]))
+        self.assertTrue(rv)
+        self.assertTrue(all(isinstance(i, tuple) for i in rv), rv)
+        self.assertEqual(
+            len([
+                v for i in list(ParserTests.Location) + list(Season)
+                for v in ([i.value] if isinstance(i.value, str) else i.value)
+            ]),
+            len(rv),
+            rv
+        )
+
+    def test_unpack_annotation_single_class(self):
+        ensemble = [ParserTests.Liquid(), ParserTests.Mass(), ParserTests.Space()]
+        rv = list(CommandParser.unpack_annotation("item", ParserTests.Liquid, ensemble))
+        self.assertTrue(rv)
+        self.assertTrue(all(isinstance(i, tuple) for i in rv), rv)
+        self.assertEqual(1, len(rv), rv)
+        self.assertIsInstance(rv[0][1], ParserTests.Liquid)
+
+    def test_unpack_annotation_dataobject(self):
+        ensemble = [ParserTests.Liquid(), ParserTests.Mass(), ParserTests.Space()]
+        rv = list(CommandParser.unpack_annotation("thing", [ParserTests.Liquid, ParserTests.Mass], ensemble))
+        self.assertTrue(all(isinstance(i, tuple) for i in rv), rv)
+        self.assertEqual(2, len(rv), rv)
+        self.assertIsInstance(rv[0][1], ParserTests.Liquid)
+        self.assertIsInstance(rv[1][1], ParserTests.Mass)
+
+    def test_expand_commands_no_preserver(self):
+
+        thing = DataObject(name="thing")
+
+        def func(obj: DataObject):
+            """
+            pick up a {obj.name}
+            """
+
+        rv = dict(CommandParser.expand_commands(func, ensemble=[thing]))
+        self.assertIn("pick up thing", rv)
+
+    def test_expand_commands_sequence_with_preserver(self):
+
+        thing = DataObject(names=["thing"])
+
+        def func(obj: DataObject):
+            """
+            pick up a {obj.names[0]}.
+            """
+
+        rv = dict(CommandParser.expand_commands(func, ensemble=[thing]))
+        self.assertIn("pick up a thing", rv)
+
+    def test_expand_commands_sequence_no_preserver(self):
+
+        thing = DataObject(names=["thing"])
+
+        def func(obj: DataObject):
+            """
+            pick up a {obj.names[0]}
+            """
+
+        rv = dict(CommandParser.expand_commands(func, ensemble=[thing]))
+        self.assertIn("pick up thing", rv)
+
+    def test_expand_commands_with_preserver(self):
+
+        thing = DataObject(name="thing")
+
+        def func(obj: DataObject):
+            """
+            pick up a {obj.name}.
+            """
+
+        rv = dict(CommandParser.expand_commands(func, ensemble=[thing]))
+        self.assertIn("pick up a thing", rv)
+
+    def test_expand_commands_discrimminator(self):
+
+        ensemble = [
+            DataObject(names=["thing"], colour="blue"),
+            DataObject(names=["thing"], colour="red"),
+        ]
+
+        def func(obj: DataObject):
+            """
+            pick up a {obj.colour} {obj.names[0]}
+            """
+
+        rv = dict(CommandParser.expand_commands(func, ensemble=ensemble))
+        self.assertIn("pick up blue thing", rv)
+        self.assertEqual("blue", rv["pick up blue thing"][1]["obj"].colour)
+        self.assertIn("pick up red thing", rv)
+        self.assertEqual("red", rv["pick up red thing"][1]["obj"].colour)
+
