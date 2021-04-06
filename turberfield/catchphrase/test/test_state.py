316d73414f6a66b3d742b9d1dee5f0ab53aca7dd
tundish <tundish@gigeconomy.org.uk>
2021-04-06 14:39:21 +0100

    Implement pulse with counters.

--- turberfield/catchphrase/test/test_state.py
+++ turberfield/catchphrase/test/test_state.py
@@ -21,6 +21,7 @@ import unittest
 #
 
 import enum
+from collections import Counter
 from collections import defaultdict
 from turberfield.dialogue.types import Stateful
 
@@ -37,17 +38,28 @@ class State:
 
 class Impulsive(Stateful):
 
-    def __init__(self, **kwargs):
+    def __init__(self, period=6, **kwargs):
         super().__init__(**kwargs)
-        self._states = defaultdict(list)
+        self._states = defaultdict(Counter)
+        self.period = period
+
+    @property
+    def pulse(self):
+        return {k: (next(iter(c.values()), 0) % self.period) or self.period for k, c in self._states.items()}
 
     def set_state(self, *args):
-        for value in args:
-            self._states[type(value).__name__].append(value)
+        for new_state in args:
+            key = type(new_state).__name__
+            old_state = self.get_state(type(new_state))
+            if new_state != old_state:
+                counter = Counter([new_state])
+                self._states[key] = counter + self._states[key]
+            else:
+                self._states[key][new_state] += 1 
         return self
 
     def get_state(self, typ=int, default=0):
-        return self._states.get(typ.__name__, [default])[-1]
+        return next(iter(self._states[typ.__name__]), default)
 
 
 class StateTests(unittest.TestCase):
@@ -60,6 +72,45 @@ class StateTests(unittest.TestCase):
         running = enum.auto()
         arrived = enum.auto()
 
-    def test_deltas(self):
-        self.fail(self.Crossing.waiting)
+    def test_compatibility(self):
+        obj = Impulsive()
+        self.assertEqual(0, obj.get_state(self.Crossing))
+        obj.state = self.Crossing.waiting
+        self.assertEqual(self.Crossing.waiting, obj.get_state(self.Crossing))
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+
+        obj.state = self.Crossing.arrived
+        self.assertEqual(self.Crossing.arrived, obj.get_state(self.Crossing))
+
+    def test_pulse(self):
+        obj = Impulsive(period=4)
+        obj.state = self.Crossing.waiting
+        self.assertEqual(self.Crossing.waiting, obj.get_state(self.Crossing))
+        self.assertEqual(1, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+        self.assertEqual(1, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+        self.assertEqual(2, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+        self.assertEqual(3, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+        self.assertEqual(4, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.walking
+        self.assertEqual(self.Crossing.walking, obj.get_state(self.Crossing))
+        self.assertEqual(1, obj.pulse["Crossing"])
+
+        obj.state = self.Crossing.arrived
+        self.assertEqual(self.Crossing.arrived, obj.get_state(self.Crossing))
+        self.assertEqual(1, obj.pulse["Crossing"])
 
